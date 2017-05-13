5a6bccab453b653d02bbda78245625957f9db1fd
Saul Zamora <szamora@pernix-solutions.com>
2017-05-12 20:15:39 -0600

    add file management

--- src/gui/main.java
+++ src/gui/main.java
@@ -10,11 +10,13 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  *
@@ -44,8 +46,11 @@ public class main extends javax.swing.JFrame {
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Compiler");
@@ -63,6 +68,22 @@ public class main extends javax.swing.JFrame {
         });
         jMenu1.add(jMenuItem2);
 
+        jMenuItem5.setText("Open file");
+        jMenuItem5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem5ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem5);
+
+        jMenuItem4.setText("Save as");
+        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem4ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem4);
+
         jMenuItem1.setText("Exit");
         jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -74,6 +95,10 @@ public class main extends javax.swing.JFrame {
         jMenuBar1.add(jMenu1);
 
         jMenu2.setText("Tools");
+
+        jMenuItem3.setText("Compile");
+        jMenu2.add(jMenuItem3);
+
         jMenuBar1.add(jMenu2);
 
         setJMenuBar(jMenuBar1);
@@ -133,6 +158,94 @@ public class main extends javax.swing.JFrame {
 
         btnClose.addActionListener(myCloseActionHandler);
     }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
+        String text;
+        if (jTabbedPane1.getSelectedComponent() == null)
+            return;
+        text = ((JTextArea) jTabbedPane1.getSelectedComponent()).getText();
+        FileFilter filter = new FileNameExtensionFilter("C Zero", "czero");
+        JFileChooser fileChooser = new JFileChooser();
+        
+        fileChooser.addChoosableFileFilter(filter);
+        int returnVal = fileChooser.showSaveDialog(this);
+        
+        if(returnVal == JFileChooser.APPROVE_OPTION) {
+            File savedFile = fileChooser.getSelectedFile();
+            String filename = savedFile.toString();
+            
+            try {
+                FileWriter writer = new FileWriter(filename);
+                BufferedWriter buffer = new BufferedWriter(writer);
+                
+                buffer.write(text);
+                
+                buffer.close();
+                writer.close();
+                
+                int tabIndex = jTabbedPane1.getSelectedIndex();
+                jTabbedPane1.setTitleAt(tabIndex, filename);
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }//GEN-LAST:event_jMenuItem4ActionPerformed
+
+    private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem5ActionPerformed
+        JFileChooser fileChooser = new JFileChooser();
+        StringBuffer buffer = new StringBuffer();
+        int resultVal = fileChooser.showOpenDialog(this);
+        
+        if (resultVal == JFileChooser.APPROVE_OPTION) {
+            File filename = fileChooser.getSelectedFile();
+            try {
+                BufferedReader bufferReader = new BufferedReader(new FileReader(filename));
+                String text = "";
+                String line;
+                
+                while((line = bufferReader.readLine()) != null) {
+                    text += line + '\n';
+                }
+                
+                bufferReader.close();
+                
+                int tabCount = jTabbedPane1.getTabCount();
+                ImageIcon icon = new ImageIcon("images/new-tab.png");
+                JTextArea textArea = new JTextArea();
+                textArea.isEditable();
+                textArea.setText(text);
+                jTabbedPane1.addTab(filename.getName(), icon, textArea, filename.getName());
+
+                int index = jTabbedPane1.indexOfTab(filename.getName());
+
+                JPanel pnlTab = new JPanel(new GridBagLayout());
+                pnlTab.setOpaque(false);
+                JLabel lblTitle = new JLabel(filename.getName());
+                JButton btnClose = new JButton("x");
+
+                GridBagConstraints gbc = new GridBagConstraints();
+                gbc.gridx = 0;
+                gbc.gridy = 0;
+                gbc.weightx = 1;
+
+                pnlTab.add(lblTitle, gbc);
+
+                gbc.gridx++;
+                gbc.weightx = 0;
+                pnlTab.add(btnClose, gbc);
+
+                jTabbedPane1.setTabComponentAt(index, pnlTab);
+
+                btnClose.addActionListener(myCloseActionHandler);
+                
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }//GEN-LAST:event_jMenuItem5ActionPerformed
     
     public class MyCloseActionHandler implements ActionListener {
         public void actionPerformed(ActionEvent evt) {
@@ -183,6 +296,9 @@ public class main extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JTabbedPane jTabbedPane1;
     // End of variables declaration//GEN-END:variables
 }
